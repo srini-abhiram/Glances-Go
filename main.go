f99533fce5c884a340b63fe431c09d1add8d7d5c
Nati-Man <nasnake7@gmail.com>
2025-10-11 18:51:00 +0300

    feat : add exporting system monitoring to csv

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
 // Handler for the /stats endpoint
@@ -19,12 +20,37 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	const exportPath = "metrics.csv" // Always use this file
+
 	// Serve static files from a 'web' directory
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/", fs)
 
 	// Handle the stats endpoint
 	http.HandleFunc("/stats", statsHandler)
+
+	log.Printf("[INFO] CSV export active → writing to %s\n", exportPath)
+	exporter := NewCSVExporter(exportPath)
+	go func() {
+		for {
+			stats, err := collectStats()
+			if err != nil {
+				log.Println("[ERROR] Collecting stats:", err)
+				time.Sleep(2 * time.Second)
+				continue
+			}
+			if err := exporter.Write(stats); err != nil {
+				log.Println("[ERROR] Writing CSV:", err)
+			}
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	http.HandleFunc("/download-csv", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Disposition", "attachment; filename=\"metrics.csv\"")
+		w.Header().Set("Content-Type", "text/csv")
+		http.ServeFile(w, r, exportPath)
+	})
 
 	log.Println("Starting server on :8080...")
 	err := http.ListenAndServe(":8080", nil)
